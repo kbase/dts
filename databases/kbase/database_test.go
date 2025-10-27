68aab4d0cd6c45fbc042632d08522d614361276b
Matt Dawson <mattldawson@gmail.com>
2025-10-27 16:25:06 -0700

    Add kbase db tests (#170)
    * pass config to kbase db constructor
    
    * add user federation tests
    
    * add kbase db tests
    
    * appease copilot
    
    * address review comments and format

--- databases/kbase/database_test.go
+++ databases/kbase/database_test.go
@@ -1,14 +1,12 @@
 package kbase
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/kbase/dts/config"
@@ -18,6 +16,8 @@ import (
 	"github.com/kbase/dts/endpoints/globus"
 )
 
+const testOrcid = "0000-0002-1825-0097"
+
 // this runs setup, runs all tests, and does breakdown
 func TestMain(m *testing.M) {
 	setup()
@@ -26,56 +26,24 @@ func TestMain(m *testing.M) {
 	os.Exit(status)
 }
 
-// runs all tests serially (so we can swap out KBase user tables)
-func TestRunner(t *testing.T) {
-	tester := SerialTests{Test: t}
-	tester.TestNewDatabase()
-	tester.TestUserFederation()
-	tester.TestSearch()
-	tester.TestResources()
-	tester.TestLocalUser()
-}
-
-func (t *SerialTests) TestNewDatabase() {
-	assert := assert.New(t.Test)
-	db, err := NewDatabase()
+func TestNewDatabase(t *testing.T) {
+	assert := assert.New(t)
+	db, err := NewDatabase(conf)
 	assert.NotNil(db, "KBase database not created")
 	assert.Nil(err, "KBase database creation encountered an error")
 }
 
-func (t *SerialTests) TestUserFederation() {
-	assert := assert.New(t.Test)
-
-	// make sure we can create a db with good user tables
-	for i := range goodUserTables {
-		err := copyDataFile(fmt.Sprintf("good_user_table_%d.csv", i), kbaseUserTableFile)
-		assert.Nil(err, "Couldn't copy good_user_table_%d.csv into place.")
-		db, err := NewDatabase()
-		assert.NotNil(db, fmt.Sprintf("KBase database not created with good_user_table_%d", i))
-		assert.Nil(err, "KBase database creation encountered an error")
-		err = stopUserFederation()
-		assert.Nil(err, "Couldn't stop user federation subsystem.")
-	}
-
-	// make sure we CAN'T create a db with bad user tables
-	for i := range badUserTables {
-		err := copyDataFile(fmt.Sprintf("bad_user_table_%d.csv", i), kbaseUserTableFile)
-		assert.Nil(err, "Couldn't copy bad_user_table_%d.csv into place.")
-		db, err := NewDatabase()
-		assert.Nil(db, fmt.Sprintf("KBase database created with bad_user_table_%d.csv", i))
-		assert.NotNil(err, "KBase database creation with bad user table didn't encounter an error")
-		err = stopUserFederation()
-		assert.Nil(err, "Couldn't stop user federation subsystem.")
-	}
-
-	// copy a good user table back into place
-	copyDataFile("good_user_table_0.csv", kbaseUserTableFile)
+func TestSpecificSearchParameters(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := NewDatabase(conf)
+	params := db.SpecificSearchParameters()
+	assert.Nil(params, "SpecificSearchParameters should return nil for kbase database")
 }
 
-func (t *SerialTests) TestSearch() {
-	assert := assert.New(t.Test)
-	orcid := os.Getenv("DTS_KBASE_TEST_ORCID")
-	db, _ := NewDatabase()
+func TestSearch(t *testing.T) {
+	assert := assert.New(t)
+	orcid := testOrcid
+	db, _ := NewDatabase(conf)
 	params := databases.SearchParameters{
 		Query: "prochlorococcus",
 		Pagination: struct {
@@ -89,31 +57,73 @@ func (t *SerialTests) TestSearch() {
 	assert.NotNil(err, "Search not implemented for kbase database!")
 }
 
-func (t *SerialTests) TestResources() {
-	assert := assert.New(t.Test)
-	orcid := os.Getenv("DTS_KBASE_TEST_ORCID")
-	db, _ := NewDatabase()
+func TestResources(t *testing.T) {
+	assert := assert.New(t)
+	orcid := testOrcid
+	db, _ := NewDatabase(conf)
 	_, err := db.Descriptors(orcid, nil)
 	assert.NotNil(err, "Descriptors not implemented for kbase database!")
 }
 
-func (t *SerialTests) TestLocalUser() {
-	assert := assert.New(t.Test)
-	db, _ := NewDatabase()
+func TestStageFiles(t *testing.T) {
+	assert := assert.New(t)
+	orcid := testOrcid
+	db, _ := NewDatabase(conf)
+	fileIds := []string{"file1", "file2"}
+	_, err := db.StageFiles(orcid, fileIds)
+	assert.NotNil(err, "StageFiles not implemented for kbase database!")
+}
+
+func TestStagingStatus(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := NewDatabase(conf)
+	id := uuid.New()
+	_, err := db.StagingStatus(id)
+	assert.NotNil(err, "StagingStatus not implemented for kbase database!")
+}
+
+func TestFinalize(t *testing.T) {
+	assert := assert.New(t)
+	orcid := testOrcid
+	db, _ := NewDatabase(conf)
+	id := uuid.New()
+	err := db.Finalize(orcid, id)
+	assert.Nil(err, "Finalize should return nil error for kbase database")
+}
+
+func TestLocalUser(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := NewDatabase(conf)
 	username, err := db.LocalUser("1234-5678-9101-112X")
 	assert.Nil(err)
 	assert.Equal("Alice", username)
 	username, err = db.LocalUser("1235-5678-9101-112X")
 	assert.NotNil(err)
 	assert.Equal("", username)
+	kbaseDb, ok := db.(*Database)
+	assert.True(ok)
+	err = kbaseDb.FinalizeDatabase()
+	assert.Nil(err)
+}
+
+func TestSaveLoad(t *testing.T) {
+	assert := assert.New(t)
+	db, _ := NewDatabase(conf)
+	state, err := db.Save()
+	assert.Nil(err, "Save should not return an error for kbase database")
+	assert.Equal("kbase", state.Name, "Save should return correct database name")
+	err = db.Load(state)
+	assert.Nil(err, "Load should not return an error for kbase database")
 }
 
 var CWD string
 var TESTING_DIR string
+var conf config.Config
 
 const kbaseConfig string = `
 service:
   data_dir: TESTING_DIR/data
+  endpoint: globus-kbase
 databases:
   kbase:
     name: KBase Workspace Service (KSS)
@@ -129,35 +139,29 @@ endpoints:
       client_secret: ${DTS_GLOBUS_CLIENT_SECRET}
 `
 
-// test user/ORCID spreadsheets
-var goodUserTables = []string{
-	`username,orcid
-Alice,1234-5678-9101-112X
-Bob,1234-5678-9101-1121
-`,
-	`orcid,username
-1234-5678-9101-112X,Alice
-1234-5678-9101-1121,Bob
-`,
-}
+// helper function replaces embedded environment variables in yaml string
+// when they don't exist in the environment
+func setTestEnvVars(yaml string) string {
+	testVars := map[string]string{
+		"DTS_GLOBUS_TEST_ENDPOINT": "6ba7b810-9dad-11d1-80b4-00c04fd430c8",
+		"DTS_GLOBUS_CLIENT_ID":     "fake_client_id",
+		"DTS_GLOBUS_CLIENT_SECRET": "fake_client_secret",
+	}
 
-var badUserTables = []string{
-	`nocommas`,
-	`orcid,orcid
-1234-5678-9101-1121,1234-5678-9101-1121
-`,
-	`username,orcid
-1234-5678-9101-1121,Bob
-Bob,1234-5678-9101-1121
-`,
-	`username,orcid
-Bob,1234-5678-9101-1121
-Bob,1234-5678-9101-1122
-`,
-	`username,orcid
-Bob,1234-5678-9101-1121
-Boberto,1234-5678-9101-1121
-`,
+	// check for existence of each variable. when not present, replace
+	// instances of it in the yaml string with a test value
+	for key, value := range testVars {
+		if os.Getenv(key) == "" {
+			yaml = os.Expand(yaml, func(yamlVar string) string {
+				if yamlVar == key {
+					return value
+				}
+				return "${" + yamlVar + "}"
+			})
+		}
+	}
+
+	return yaml
 }
 
 // this function gets called at the begіnning of a test session
@@ -179,44 +183,20 @@ func setup() {
 
 	// read the config file with TESTING_DIR replaced
 	myConfig := strings.ReplaceAll(kbaseConfig, "TESTING_DIR", TESTING_DIR)
-	config.Init([]byte(myConfig))
-
-	// create the data directory and populate it with our test spreadsheets
-	os.Mkdir(config.Service.DataDirectory, 0755)
-	for i, userTable := range goodUserTables {
-		filename := filepath.Join(config.Service.DataDirectory, fmt.Sprintf("good_user_table_%d.csv", i))
-		file, _ := os.Create(filename)
-		io.WriteString(file, userTable)
-		file.Close()
+	myConfig = setTestEnvVars(myConfig)
+	err = config.Init([]byte(myConfig))
+	if err != nil {
+		log.Panicf("Couldn't initialize config: %s", err)
 	}
-	for i, userTable := range badUserTables {
-		filename := filepath.Join(config.Service.DataDirectory, fmt.Sprintf("bad_user_table_%d.csv", i))
-		file, _ := os.Create(filename)
-		io.WriteString(file, userTable)
-		file.Close()
+	conf, err = config.NewConfig([]byte(myConfig))
+	if err != nil {
+		log.Panicf("Couldn't parse config: %s", err)
 	}
 
-	// copy a good user table into place
-	copyDataFile("good_user_table_0.csv", kbaseUserTableFile)
+	setupUserFederationTests(config.Service.DataDirectory)
 
-	databases.RegisterDatabase("kbase", NewDatabase)
+	databases.RegisterDatabase("kbase", DatabaseConstructor(conf))
 	endpoints.RegisterEndpointProvider("globus", globus.NewEndpointFromConfig)
-}
-
-// copies a file from a source to a destination file within the DTS data directory
-func copyDataFile(src, dst string) error {
-	srcFile, err := os.Open(filepath.Join(config.Service.DataDirectory, src))
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-	dstFile, err := os.Create(filepath.Join(config.Service.DataDirectory, dst))
-	if err != nil {
-		return err
-	}
-	defer dstFile.Close()
-	_, err = io.Copy(dstFile, srcFile)
-	return err
 }
 
 // this function gets called after all tests have been run
@@ -227,7 +207,3 @@ func breakdown() {
 		os.RemoveAll(TESTING_DIR)
 	}
 }
-
-// To run the tests serially, we attach them to a SerialTests type and
-// have them run by a a single test runner.
-type SerialTests struct{ Test *testing.T }

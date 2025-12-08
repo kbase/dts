b1a2982d5e328bb5ffcbedcc675891d4da762aed
Jeffrey N. Johnson <jeff@cohere-llc.com>
2025-12-08 11:41:31 -0800

    Bumped minor version number (should've happened already).

--- services/version.go
+++ services/version.go
@@ -7,7 +7,7 @@ import (
 // Version numbers
 var majorVersion = 0
 var minorVersion = 10
-var patchVersion = 1
+var patchVersion = 2
 
 // Version string
 var version = fmt.Sprintf("%d.%d.%d", majorVersion, minorVersion, patchVersion)

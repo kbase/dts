9fda4437a8a40091f25e49dae4ae373e47326a4b
Jeffrey N. Johnson <jeff@cohere-llc.com>
2026-06-03 11:04:02 -0700

    Updating some Action versions and disabling KBase auth2 server tests (#222)
    * Bumping some action versions.
    
    * Disabling KBase auth2 server tests (disallowed by Cloudflare bot storm security)
    
    * Moving t.Skip statements upward.

--- auth/kbase_auth_server_test.go
+++ auth/kbase_auth_server_test.go
@@ -136,6 +136,7 @@ func getKbaseToken() (string, bool) {
 // tests whether a proxy for the KBase authentication server can be
 // constructed
 func TestNewKBaseAuthServer(t *testing.T) {
+	t.Skip("Skipping KBase auth2 server test (access denied by Cloudflare bot storm security)")
 	assert := assert.New(t)
 
 	devToken, ok := getKbaseToken()
@@ -158,6 +159,7 @@ func TestNewKBaseAuthServer(t *testing.T) {
 // tests whether an invalid KBase token prevents a proxy for the auth server
 // from being constructed
 func TestInvalidToken(t *testing.T) {
+	t.Skip("Skipping KBase auth2 server test (access denied by Cloudflare bot storm security)")
 	assert := assert.New(t)
 
 	_, ok := getKbaseToken()
@@ -180,6 +182,7 @@ func TestInvalidToken(t *testing.T) {
 
 // tests that the proxy handles missing identifiers correctly
 func TestNoIdentifiers(t *testing.T) {
+	t.Skip("Skipping KBase auth2 server test (access denied by Cloudflare bot storm security)")
 	assert := assert.New(t)
 
 	_, ok := getKbaseToken()
@@ -198,6 +201,7 @@ func TestNoIdentifiers(t *testing.T) {
 
 // tests that the proxy handles missing OrcID identifiers correctly
 func TestNoOrcID(t *testing.T) {
+	t.Skip("Skipping KBase auth2 server test (access denied by Cloudflare bot storm security)")
 	assert := assert.New(t)
 
 	_, ok := getKbaseToken()
@@ -217,6 +221,7 @@ func TestNoOrcID(t *testing.T) {
 // tests whether the authentication server can return information for the
 // client (the user associated with the specified developer token)
 func TestClient(t *testing.T) {
+	t.Skip("Skipping KBase auth2 server test (access denied by Cloudflare bot storm security)")
 	assert := assert.New(t)
 
 	devToken, ok := getKbaseToken()
